afdfc3b1f5c95292b8ab4b521d510e42b2a8e653
abdalrhamn kreshan <tech.engelectrical@gmail.com>
2024-06-22 17:38:56 +0300

    Port & host try 2

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,13 @@ import cookieParaser from "cookie-parser";
 import log from "./utils/logger";
 import connect from "./utils/connect";
 import router from "./routes/index";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 const app = express();
 
 dotenv.config();
-const port = parseInt(process.env.PORT || '3000') 
-const host = process.env.HOST || 'localhost'
+const port = parseInt(process.env.PORT || "3000");
+const host = process.env.HOST || "localhost";
 app.use(express.json());
 
 app.use(
@@ -22,12 +22,13 @@ app.use(
 app.use(cookieParaser());
 app.use(router);
 
-app.get("/", (req, res)=> {
-  res.send('<h1 style="text-align:center; margin-top:5%;">VoteTracker-app (Backend Node js)</h1>')
-})
+app.get("/", (req, res) => {
+  res.send(
+    '<h1 style="text-align:center; margin-top:5%;">VoteTracker-app (Backend Node js)</h1>'
+  );
+});
 
-
-app.listen(port, host,() => {
+app.listen(port, host, () => {
   log.info(`Server is running at localhost ${process.env.port}`);
 
   connect();
