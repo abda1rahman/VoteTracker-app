7daa44f45a4d135a6cac57efbf09ae60c20d53c3
abdalrhamn kreshan <tech.engelectrical@gmail.com>
2024-08-14 18:38:23 +0300

    small change 2

--- src/redis/index.ts
+++ src/redis/index.ts
@@ -4,9 +4,13 @@ import log from '../utils/logger';
 // for production use { url: "redis://localhost:6379" }
 const client = createClient(
   {
-    url: "redis://127.0.0.1:6379"
-    
-  })
+    // url: "redis://localhost:6379"
+  password: "5DL7BSECvX6yH37kBdW9fsfVdm3oqM6r",
+  socket: {
+    host: "redis-16438.c212.ap-south-1-1.ec2.redns.redis-cloud.com",
+    port: 16438
+  }
+})
 
 client.on('error', err => console.log('Redis Client Error', err));
 
