2cab761622837b259d92381d15b02c965717a594
abdalrhamn kreshan <tech.engelectrical@gmail.com>
2024-08-14 21:47:55 +0300

    small change 9

--- src/redis/index.ts
+++ src/redis/index.ts
@@ -1,27 +1,29 @@
-import {createClient} from 'redis'
-import log from '../utils/logger';
+import { createClient } from "redis";
+import log from "../utils/logger";
 
 // for production use { url: "redis://localhost:6379" }
-const client = createClient()
+const client = createClient({
+  url: "redis://127.0.0.1:6379",
+});
 
-client.on('error', err => console.log('Redis Client Error', err));
+client.on("error", (err) => console.log("Redis Client Error", err));
 
-(async()=> {
+(async () => {
   try {
     await client.connect();
-  } catch (error:any) {
-    log.error('Error connection to Redis')
-    process.exit(1)
+  } catch (error: any) {
+    log.error("Error connection to Redis");
+    process.exit(1);
   }
-})()
+})();
 
-client.on('connect', ()=> {
-  log.info('connection with Redis')
-})
+client.on("connect", () => {
+  log.info("connection with Redis");
+});
 
-process.on('SIGINT', async () => {
+process.on("SIGINT", async () => {
   await client.quit();
   process.exit();
 });
 
-export default client
+export default client;
