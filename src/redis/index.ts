4d97f58b220d0a164d10b9e906b923a023d4e5b9
abdalrhamn kreshan <tech.engelectrical@gmail.com>
2024-08-14 20:23:37 +0300

    small change 6

--- src/redis/index.ts
+++ src/redis/index.ts
@@ -2,15 +2,7 @@ import {createClient} from 'redis'
 import log from '../utils/logger';
 
 // for production use { url: "redis://localhost:6379" }
-const client = createClient(
-  {
-    // url: "redis://localhost:6379"
-  password: "5DL7BSECvX6yH37kBdW9fsfVdm3oqM6r",
-  socket: {
-    host: "redis-16438.c212.ap-south-1-1.ec2.redns.redis-cloud.com",
-    port: 16438
-  }
-})
+const client = createClient()
 
 client.on('error', err => console.log('Redis Client Error', err));
 
