a66d916ddf99d75cab430580076a1018a6a864a0
abdalrhamn kreshan <tech.engelectrical@gmail.com>
2024-08-15 00:36:46 +0300

    try 3

--- src/redis/index.ts
+++ src/redis/index.ts
@@ -4,7 +4,7 @@ import log from "../utils/logger";
 // for production use { url: "redis://localhost:6379" }
 const client = createClient({
   socket: {
-    host: "127.0.0.1",
+    host: "164.92.145.147",
     port: 6379,
   },
 });
