a3e05e3add17fd65fd87eb8aaa35c1eb2a4f571d
abdalrhamn kreshan <tech.engelectrical@gmail.com>
2024-08-15 22:10:35 +0300

    try 7-1

--- src/redis/index.ts
+++ src/redis/index.ts
@@ -2,10 +2,11 @@ import { createClient } from "redis";
 import log from "../utils/logger";
 
 // for production use { url: "redis://localhost:6379" }
-const host = process.env.NODE_ENV === 'production' ? process.env.REDIS_HOST : 'localhost'
+const host =
+  process.env.NODE_ENV === "production" ? process.env.REDIS_HOST : "localhost";
 const client = createClient({
   socket: {
-    host: 'redis-stack',
+    host: "redis",
     port: 6379,
   },
 });
